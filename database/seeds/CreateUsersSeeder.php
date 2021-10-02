27a47137d708e4f25572c0db36ce6368b5d5fde5
ahmedhassan239 <ahmedhassanelsayed239@gmail.com>
2021-10-02 21:50:10 +0200

    fix bug

--- database/seeds/CreateUsersSeeder.php
+++ database/seeds/CreateUsersSeeder.php
@@ -21,12 +21,6 @@ class CreateUsersSeeder extends Seeder
                 'is_admin'=>'1',
                 'password'=> bcrypt('123456'),
             ],
-            [
-                'name'=>'User',
-                'email'=>'merchant@merchant.com',
-                'is_admin'=>'0',
-                'password'=> bcrypt('123456'),
-            ],
         ];
 
         foreach ($user as $key => $value) {
